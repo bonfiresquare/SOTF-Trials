49dc9ba43cc0f83f1b8972cb77ac6747654d640a
gnada <d.gnauck@mailbox.org>
2016-11-27 01:47:28 +0100

    got rid of some functions and simplified some others.

--- app/Minder.py
+++ app/Minder.py
@@ -10,52 +10,44 @@ from Window import *
 
 class Minder(ABC):
     _mouse_move = ()
-
+    _keymap = {pygame.K_ESCAPE: 'QUIT',
+               pygame.K_LEFT:   'MOVE_LEFT',
+               pygame.K_RIGHT:  'MOVE_RIGHT',
+               pygame.K_UP:     'MOVE_UP',
+               pygame.K_DOWN:   'MOVE_DOWN',
+               pygame.K_F2:     'RESET',
+               pygame.K_F3:     'CLEAR',
+               pygame.K_F5:     'DRAW',
+               pygame.K_F8:     'SAVE',
+               pygame.K_F9:     'LOAD',
+               pygame.K_F11:    'TOGGLE_FULLSCREEN',
+               pygame.K_F12:    'CAPTURE'}
     @staticmethod
     def have_a_look():
         output = ''
-        for event in pygame.event.get():
+        for e in pygame.event.get():
             # print(event)
-            if event.type == pygame.QUIT:
+            if e.type == pygame.QUIT:
                 output = 'QUIT'
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.event.post(pygame.event.Event(pygame.QUIT))
-                elif event.key == pygame.K_LEFT:
-                    output = 'MOVE_LEFT'
-                elif event.key == pygame.K_RIGHT:
-                    output = 'MOVE_RIGHT'
-                elif event.key == pygame.K_UP:
-                    output = 'MOVE_UP'
-                elif event.key == pygame.K_DOWN:
-                    output = 'MOVE_DOWN'
-                elif event.key == pygame.K_F2:
-                    output = 'RESET'
-                elif event.key == pygame.K_F3:
-                    output = 'CLEAR'
-                elif event.key == pygame.K_F5:
-                    output = 'DRAW'
-                elif event.key == pygame.K_F11:
-                    output = 'TOGGLE_FULLSCREEN'
-                elif event.key == pygame.K_F12:
-                    output = 'CAPTURE'
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
+            elif e.type == pygame.KEYDOWN:
+                output = Minder._keymap[e.key]
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                if e.button == 1:
                     pygame.mouse.set_cursor(*Window.get_cursor_data('lmb'))
-                elif event.button == 3:
+                elif e.button == 3:
                     pygame.mouse.set_cursor(*Window.get_cursor_data('rmb'))
-                elif event.button == 4:
+                elif e.button == 4:
                     output = 'ZOOM_IN'
-                elif event.button == 5:
+                elif e.button == 5:
                     output = 'ZOOM_OUT'
-            elif event.type == pygame.MOUSEMOTION:
+            elif e.type == pygame.MOUSEMOTION:
                 Minder._mouse_move = pygame.mouse.get_rel()
                 if pygame.mouse.get_pressed()[2]:
                     pygame.mouse.set_cursor(*Window.get_cursor_data('drg'))
                     output = 'DRAGGING'
-            elif event.type == pygame.MOUSEBUTTONUP:
+            elif e.type == pygame.MOUSEBUTTONUP:
                 pygame.mouse.set_cursor(*Window.get_cursor_data('std'))
-                if event.button == 3:
+                if e.button == 3:
                     output = 'DRAGGING_OFF'
         return output
 
--- app/Params.py
+++ app/Params.py
@@ -13,36 +13,26 @@ import pygame
 class Params(ABC):
 
     window_size = (1200, 800)
-    map_size = (600, 400)   # default = (250, 250)
-    map_tilesize = 1          # default = 10
-    map_stepping = 32        # default = 10
-    map_freq_multiplier = 0.9 # default = 0.8
-    map_octaves = 64        # default = 64
+    map_size = (500, 500)       # default = (250, 250)
+    map_tilesize = 2            # default = 10
+    map_stepping = 16           # default = 10
+    map_freq_multiplier = 0.7   # default = 0.8
+    map_octaves = 64            # default = 64
 
-    map_min_tilesize = 1      # default = 2
-    map_max_tilesize = 40     # default = 40
+    map_min_tilesize = 1        # default = 2
+    map_max_tilesize = 20       # default = 40
 
     # height < map_waterlevel = water
     # height < map_grasslevel && > map_waterlevel = grass
     # height > map_grasslevel = mountain
-    map_waterlevel = ceil(map_stepping * 0.5)  # default = ceil(map_stepping * 0.5)
-    map_grasslevel = ceil(map_stepping * 0.70)   # default = ceil(map_stepping * 0.75)
+    map_waterlevel = ceil(map_stepping * 0.45)  # default = ceil(map_stepping * 0.5)
+    map_grasslevel = ceil(map_stepping * 0.65)   # default = ceil(map_stepping * 0.75)
 
     map_current_offset = (0, 0)
     map_add_surface_offset = (0, 0)  # additional surface offset (relative to current offset)
     win_render_margin = 40           # rendered margin around the visible map - needed to avoid missing tiles
 
     @staticmethod
-    def calc_min_tilesize(_size):
-        # Params.map_min_tilesize = Tools.clip(max(ceil((_size[0] / Params.map_size[0]) / 2) * 2,
-        #                                          ceil((_size[1] / Params.map_size[1]) / 2) * 2), 4, 40)
-        if Params.map_tilesize < Params.map_min_tilesize:
-            Params.map_tilesize = Params.map_min_tilesize
-
-    @staticmethod
-    def increase_tilesize():
-            Params.map_tilesize = Tools.clip(Params.map_tilesize + 1, Params.map_min_tilesize, Params.map_max_tilesize)
-
-    @staticmethod
-    def decrease_tilesize():
-            Params.map_tilesize = Tools.clip(Params.map_tilesize - 1, Params.map_min_tilesize, Params.map_max_tilesize)
+    def scale_tilesize(_whereto):
+        _delta = 1 if _whereto == 'ZOOM_IN' else -1  # if _whereto == 'ZOOM_OUT'
+        Params.map_tilesize = Tools.clip(Params.map_tilesize + _delta, Params.map_min_tilesize, Params.map_max_tilesize)
